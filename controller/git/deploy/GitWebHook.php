7c71ed10d999fd51d6971e247edbaa5ec5d867e2
genghongfei <genghongfei@suule.com>
2018-03-10 22:51:39 +0800

    增加对GItWebHook功能

---
+++ controller/git/deploy/GitWebHook.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @Author: 耿鸿飞<genghongfei@suule.com>
+ * @Date: 2018/3/10 下午10:49
+ */
+
+
+namespace controller\git\deploy;
+
+
+class GitWebHook extends \g\Controller
+{
+    public function get(){
+
+    }
+}
