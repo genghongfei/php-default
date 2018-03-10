96922e18c1dd8580787c479d8a0b5e89fa1ccd01
genghongfei <genghongfei@suule.com>
2018-03-10 23:01:20 +0800

    fx

---
+++ controller/git/nginx/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @Author: 耿鸿飞<genghongfei@suule.com>
+ * @Date: 2018/3/10 下午11:00
+ */
+
+
+namespace controller\git\nginx;
+
+
+class Index extends \g\Controller
+{
+    public function get(){
+        
+    }
+}
